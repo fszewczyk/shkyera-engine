ac7fdb84dcce3dddc52c2f95c7db6ba924bca457
Franciszek Szewczyk <szewczyk.franciszek02@gmail.com>
2024-11-10 15:21:49 +0100

    Imrpvoed cascade borders

--- src/ui/UI.cpp
+++ src/ui/UI.cpp
@@ -54,8 +54,8 @@ void UI::initializeImgui() {
 #elif defined(__APPLE__)
   // GL 3.2 + GLSL 150
   const char* glsl_version = "#version 150";
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE,
                  GLFW_OPENGL_CORE_PROFILE);             // 3.2+ only
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
